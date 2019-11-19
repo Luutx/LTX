6ca94c58849554b88e0821b4591ea5cf1d46a963
Luutx <1206009130@qq.com>
2019-11-19 14:35:20 +0800

    week7

--- week1/code/my.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <wait.h>
-#include <signal.h>
-
-
--- week9/code/day1/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-int main()
-{
-	printf("tset:pid=%d,ppid=%d\n",getpid(),getppid());
-}
